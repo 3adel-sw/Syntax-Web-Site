a7d63c9f4c7d7b16ebe2be829694cf66896fd31f
Adel Mahmoud <149318365+3adel-sw@users.noreply.github.com>
2026-05-15 00:34:02 +0300

    Update ..

--- src/components/Ui/Menu.jsx
+++ src/components/Ui/Menu.jsx
@@ -20,6 +20,8 @@ const pages = [
   { labelKey: "menu.pagesList.resources", path: "/resources" },
   { labelKey: "menu.pagesList.blogs", path: "/blogs" },
   { labelKey: "menu.pagesList.b2bTraining", path: "/b2b" },
+  { labelKey: "menu.pagesList.events", path: "/events" }
+ 
 ];
 
 
